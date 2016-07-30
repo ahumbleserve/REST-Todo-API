1f00c03969f25c9309121613ad2fb953744c40de
Vitor Jesus <ahumbleserve@yahoo.com.br>
2016-07-30 11:07:02 -0300

    update server with static list of todos

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-
+var todos = [
+	{
+		id:1,
+		description:'Meet mom for lunch',
+		completed:false
+	},
+	{
+		id:2,
+		description:'Go to market',
+		completed:false
+	}
+]
 
 app.get('/',function (req, res){
 	res.send('Todo API Root');
 })
 
+// GET /todos
+app.get('/todos',function (req, res){
+	res.json(todos);
+})
 
+// GET /todos/:id
+app.get('/todos/:id',function (req, res){
+	res.send(todos[req.id]);
+})
+
+
+//Setting port and listenning
 app.listen(PORT, function (){
 	console.log('Express listenning on port '+PORT+'!');
 });
