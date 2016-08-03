ba054d4cbd94082f21bbae782a46280952df4c46
Vitor Jesus <ahumbleserve@yahoo.com.br>
2016-08-03 01:10:10 -0300

    update methods fro sequelize

--- db.js
+++ db.js
@@ -3,11 +3,12 @@ var env = process.env.NODE_ENV || 'development';
 var  sequelize;
 
 if(env === 'production'){
+	//heroku
 	sequelize = new Sequelize(process.env.DATABASE_URL,{
 		dialect:'postgres'
 	});
 }else{
-	
+	//aws
 	sequelize = new Sequelize(undefined, undefined, undefined, {
 	'dialect': 'sqlite',
 	'storage': __dirname + '/data/dev-todo-api.sqlite'
