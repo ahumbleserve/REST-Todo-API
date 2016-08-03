1dfe7ba0942af9d95459cb469d8ad6ad813e3943
Vitor Jesus <ahumbleserve@yahoo.com.br>
2016-08-03 15:55:20 -0300

    generated token

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
@@ -46,6 +48,24 @@ module.exports = function(sequelize, DataTypes) {
 			toPublicJSON:function(){
 				var json = this.toJSON();
 				return _.pick(this, 'id', 'email','createdAt','updatedAt');
+			},
+			generateToken:function(type){
+				if(!_.isString(type)){
+					return undefined;
+				}
+
+				try{
+					var stringData = JSON.stringify({id: this.get('id'),type:type});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123').toString();
+					var token = jwt.sign({
+						token:encryptedData
+					},'dfg456');
+
+					return token;
+				}catch(e){
+					console.log(e);
+					return undefined;
+				}
 			}
 		},
 		classMethods:{
